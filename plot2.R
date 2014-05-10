b4d2bcd41248a232ebcac01858352c45bd084e3d
Florian Knaus <florian.knaus@gmail.com>
2014-05-10 12:24:08 -0500

    Plot 2

---
+++ plot2.R
@@ -0,0 +1,21 @@
+# set working directory
+setwd("C:/Users/10131433/Dropbox/eLearning/Data Science/4 Exploratory Data Analysis/W1");
+# read data
+housepower <- read.table(file="household_power_consumption.txt", header=TRUE, sep=";", colClasses=c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'),na.strings="?");
+# create date-time variable by combining Date and Time column
+housepower$datetime <- strptime(paste(housepower$Date, housepower$Time), "%d/%m/%Y %H:%M:%S");
+housepower$Date <- as.Date(housepower$Date, "%d/%m/%Y");
+## analysis of data during development
+# summary(housepower);
+# str(housepower);
+# head(housepower, n=3);
+# get subset of data between Feb 01 and Feb 02 2007
+data <- subset(housepower, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"));
+# open png device
+png(filename="plot2.png", width=480, height=480)
+# create empty plot based on subset 
+plot(data$datetime, data$Global_active_power, xlab="", ylab="Global Active Power (kilowats)", main="", type="n");
+# plot line
+lines(data$datetime, data$Global_active_power);
+# close device
+dev.off();
